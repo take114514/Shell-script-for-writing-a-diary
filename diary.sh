5b1bb92df72f103fdd612e79e123fc9f7bb9ec2b
ryuya takei <maibadilin@gmail.com>
2021-09-17 23:22:04 +0900

    diary.sh2

--- diary.sh
+++ diary.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-vim $(date '+%Y-%m-%d').txt
+#日記データの保存ディレクトリ
+directory="${HOME}/diary"
+
+#データ保存ディレクトリがなければ作成する
+if [ ! -d "$directory" ];then
+mkdir "$directory"
+fi
+
+vim "${directory}/$(date '+%Y-%m-%d').txt
