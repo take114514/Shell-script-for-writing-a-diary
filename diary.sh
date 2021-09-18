051b7daf49ff0331e0f953323455701ef5e06203
ryuya takei <maibadilin@gmail.com>
2021-09-18 20:51:21 +0900

    diary.sh3

--- diary.sh
+++ diary.sh
@@ -8,4 +8,12 @@ if [ ! -d "$directory" ];then
 mkdir "$directory"
 fi
 
+#日記ファイルパスの組み立て
+diaryfile="${directory}/$(date '+%Y-%m-%d').txt"
+
+#日記ファイルがなければ先頭に日記を挿入
+if [ ! -e "diaryfile" ];then
+date '+%Y-%m-%d' > "$diaryfile"
+fi
+
 vim "${directory}/$(date '+%Y-%m-%d').txt
