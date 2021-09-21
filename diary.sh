11f6c8c7bad65fb93dfa44b9c21523e652f24e51
ryuya takei <maibadilin@gmail.com>
2021-09-21 18:51:59 +0900

    再度コメントアウト

--- diary.sh
+++ diary.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #日記データの保存ディレクトリの引数
-directory="${HOME}/diary"
+#directory="${HOME}/diary"
 
 #データ保存ディレクトリがなければホームディレクトリ直下に作成する
-if [ ! -d "$directory" ];then
-mkdir "$directory"
-fi
+#if [ ! -d "$directory" ];then
+#mkdir "$directory"
+#fi
 
 #日記ファイルパスの組み立て
-diaryfile="${directory}/$(date '+%Y-%m-%d').txt"
+diaryfile="$(date '+%Y-%m-%d').txt"
 
 #日記ファイルがなければ先頭に日記を挿入
 if [ ! -e "$diaryfile" ];then
