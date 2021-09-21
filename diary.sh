901029691465264caf8fc4442a689061377eed24
ryuya takei <maibadilin@gmail.com>
2021-09-21 18:44:54 +0900

    コメントアウト部分の修正

--- diary.sh
+++ diary.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #日記データの保存ディレクトリの引数
-#directory="${HOME}/diary"
+qdirectory="${HOME}/diary"
 
 #データ保存ディレクトリがなければホームディレクトリ直下に作成する
-#if [ ! -d "$directory" ];then
-#mkdir "$directory"
-#fi
+if [ ! -d "$directory" ];then
+mkdir "$directory"
+fi
 
 #日記ファイルパスの組み立て
 diaryfile="${directory}/$(date '+%Y-%m-%d').txt"
 
 #日記ファイルがなければ先頭に日記を挿入
-if [ ! -e "diaryfile" ];then
+if [ ! -e "$diaryfile" ];then
 date '+%Y-%m-%d' > "$diaryfile"
 fi
 
 #vimの起動および"$diaryfile"に実行権限を付与する
-vim "$diaryfile" && chmod 755 ""$diaryfile""
+vim "$diaryfile" && chmod 755 "$diaryfile"
